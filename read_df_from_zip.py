30886401d2088d7cdd62c59905b2d03579dd0b75
Giu <giuliaghisolfi99@gmail.com>
2024-04-10 15:45:13 +0200

    Add files via upload

--- read_df_from_zip.py
+++ read_df_from_zip.py
@@ -1,12 +1,24 @@
 import os
+import pickle
 import zipfile
+
 import pandas as pd
 
-def load_df(df_zip_path):
+
+def load_df(df_zip_path, file_format='pickle'):
     with zipfile.ZipFile(df_zip_path, 'r') as zip_ref:
         folder_path = os.path.dirname(df_zip_path)
         zip_ref.extractall(folder_path)
-    csv_path = df_zip_path.replace('.zip', '.csv')
-    df = pd.read_csv(csv_path)
+
+    if file_format == 'pickle':
+        file_ext = '.pkl'
+        file_path = df_zip_path.replace('.zip', file_ext)
+        with open(file_path, 'rb') as file:
+            df = pickle.load(file)
+    elif file_format == 'csv':
+        file_ext = '.csv'
+        file_path = df_zip_path.replace('.zip', file_ext)
+        df = pd.read_csv(file_path)
 
     return df
+
